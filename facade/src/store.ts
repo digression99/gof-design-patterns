95d6a775e801279baee9c07799c39e01eed1c108
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2020-08-01 22:10:45 +0900

    Add reducer and actions

--- facade/src/store.ts
+++ facade/src/store.ts
@@ -1,9 +1,31 @@
 import { createStore } from "redux";
 import defaultImage from "./question-mark.jpg";
 
+export const selectImage = (url) => ({ type: "SELECT_IMAGE", payload: url });
+
+export const setFilterNoise = (noiseValue) => ({
+  type: "SET_FILTER_NOISE",
+  payload: noiseValue,
+});
+
+export const setFilterHue = (hueValue) => ({
+  type: "SET_FILTER_HUE",
+  payload: hueValue,
+});
+
+export const setFilterRipple = (rippleValue) => ({
+  type: "SET_FILTER_RIPPLE",
+  payload: rippleValue,
+});
+
 const initialState = {
-  imageUrl: defaultImage,
+  selectedImageUrl: defaultImage,
   croppedImageUrl: "",
+  filters: {
+    noise: 0,
+    hue: 0,
+    ripple: 0,
+  },
 };
 
 const reducer = (state = initialState, action) => {
@@ -14,6 +36,28 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         croppedImageUrl: action.payload,
+      };
+    }
+
+    case "SELECT_IMAGE": {
+      return {
+        ...state,
+        selectedImageUrl: action.payload,
+      };
+    }
+
+    case "SET_FILTER_NOISE": {
+      return { ...state, filters: { ...state.filters, noise: action.payload } };
+    }
+
+    case "SET_FILTER_HUE": {
+      return { ...state, filters: { ...state.filters, hue: action.payload } };
+    }
+
+    case "SET_FILTER_RIPPLE": {
+      return {
+        ...state,
+        filters: { ...state.filters, ripple: action.payload },
       };
     }
     default:
