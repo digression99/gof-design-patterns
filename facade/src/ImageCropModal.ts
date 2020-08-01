76113d718e982cdf53c8b8fe42f88ce8c4e757a7
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2020-08-01 22:12:16 +0900

    Refactor ImageCropModal

--- facade/src/ImageCropModal.ts
+++ facade/src/ImageCropModal.ts
@@ -1,26 +1,24 @@
 import { ImageSelector } from "./ImageSelector";
 import { ImageCropper } from "./ImageCropper";
 import { ImageFilter } from "./ImageFilter";
-import { RangeSlider } from "./RangeSlider";
+import { store } from "./store";
 
 export class ImageCropModal {
   constructor(
     public imageSelector: ImageSelector = null,
     public imageCropper: ImageCropper = null,
-    public imageFilter: ImageFilter = null,
-    public rangeSlider: RangeSlider = null
+    public imageFilter: ImageFilter = null
   ) {
     this.imageSelector = new ImageSelector();
     this.imageCropper = new ImageCropper();
     this.imageFilter = new ImageFilter();
-    this.rangeSlider = new RangeSlider();
 
     const filterButton = document.getElementById("filter-button");
 
     filterButton.addEventListener("click", () => {
-      this.imageCropper
-        .getCroppedImage()
-        .then(this.imageFilter.applyFilter.bind(this.imageFilter));
+      const state = store.getState();
+
+      this.imageFilter.applyFilter(state.selectedImageUrl);
     });
   }
 }
